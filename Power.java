20c5c40d1a5be2a53179e911789adb0023d2f148
tianyi7 <1141157984@qq.com>
2017-08-09 09:03:00 +0800

    数值的整数次方

---
+++ Power.java
@@ -0,0 +1,14 @@
+/**
+ * Author: tianyi
+ * Time: 2017/8/9
+ * Question:给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ * Source:剑指Offer
+ */
+import java.util.*;
+public class Power {
+    public double Power(double base, int exponent) {
+        double result=Math.pow(base,exponent);
+        return result;
+
+    }
+}
